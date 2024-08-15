2e16ac3b5735622bf767b5be47533d41741013ff
mohanad <mohanadhassany4@gmail.com>
2024-08-15 22:02:57 +0300

    ss

--- pharmacy_clients/lib/views/widgets/client_transactions.dart
+++ pharmacy_clients/lib/views/widgets/client_transactions.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -70,26 +72,34 @@ class _ClientTransactionsState extends State<ClientTransactions> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
+                      Column(
                         children: [
-                          // Text(
-                          //     "${firstDate?.day}/${firstDate?.month}/${firstDate?.year}"),
-                          IconButton(
-                            onPressed: pickFirstDate,
-                            icon: const Icon(Icons.calendar_today),
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: pickFirstDate,
+                                icon: const Icon(Icons.calendar_today),
+                              ),
+                              const Text(" من تاريخ")
+                            ],
                           ),
-                          const Text("من")
+                          Text(
+                              "${firstDate?.day}/${firstDate?.month}/${firstDate?.year}"),
                         ],
                       ),
-                      Row(
+                      Column(
                         children: [
-                          // Text(
-                          //     "${secDate?.day}/${secDate?.month}/${secDate?.year}"),
-                          const Text("الى"),
-                          IconButton(
-                            onPressed: pickSecDate,
-                            icon: const Icon(Icons.calendar_today),
+                          Row(
+                            children: [
+                              const Text("الى تاريخ"),
+                              IconButton(
+                                onPressed: pickSecDate,
+                                icon: const Icon(Icons.calendar_today),
+                              ),
+                            ],
                           ),
+                          Text(
+                              "${secDate?.day}/${secDate?.month}/${secDate?.year}"),
                         ],
                       ),
                     ],
@@ -97,6 +107,90 @@ class _ClientTransactionsState extends State<ClientTransactions> {
                   const SizedBox(
                     height: 20,
                   ),
+                  const Divider(),
+                  StreamBuilder<QuerySnapshot>(
+                      stream: firstDate != null && secDate != null
+                          ? FirebaseFirestore.instance
+                              .collection("users")
+                              .doc(FirebaseAuth.instance.currentUser!.uid)
+                              .collection("clients")
+                              .doc()
+                              .collection("invoices")
+                              .orderBy("date", descending: true)
+                              .where("date",
+                                  isGreaterThanOrEqualTo: firstDate,
+                                  isLessThanOrEqualTo: secDate)
+                              //.where("date", isLessThanOrEqualTo: secDate)
+                              .snapshots()
+                          : FirebaseFirestore.instance
+                              .collection("users")
+                              .doc(FirebaseAuth.instance.currentUser!.uid)
+                              .collection("clients")
+                              .doc()
+                              .collection("invoices")
+                              .orderBy("date", descending: true)
+                              .snapshots(),
+                      builder:
+                          (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator(
+                            backgroundColor: Colors.deepPurple,
+                          ));
+                        }
+                        if (snapshot.hasError) {
+                          return Center(
+                            child: Text("Some error occured ${snapshot.error}"),
+                          );
+                        } else {
+                          return ListView.builder(
+                              scrollDirection: Axis.vertical,
+                              itemCount: snapshot.data!.docs.length,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Card(
+                                      elevation: 5,
+                                      child: ListTile(
+                                        leading: Image.network(
+                                          "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+                                          width: 40,
+                                          height: 50,
+                                          fit: BoxFit.cover,
+                                        ),
+                                        title: Text(
+                                          "${snapshot.data!.docs[index]['name']}",
+                                        ),
+                                        subtitle: RichText(
+                                            text: TextSpan(children: [
+                                          TextSpan(
+                                              text:
+                                                  "${snapshot.data!.docs[index]['type']}- ",
+                                              style: const TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold)),
+                                          TextSpan(
+                                              text: snapshot.data!.docs[index]
+                                                  ['date'],
+                                              style: const TextStyle(
+                                                  color: Colors.deepPurple,
+                                                  fontWeight: FontWeight.bold))
+                                        ])),
+                                        trailing: Text(
+                                          "${snapshot.data!.docs[index]['cureAmount']}",
+                                          style: const TextStyle(
+                                              color: Colors.green,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18),
+                                        ),
+                                      ),
+                                    ));
+                              });
+                        }
+                      })
                 ],
               )),
         ),
